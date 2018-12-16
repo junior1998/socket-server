065f8080b17ad8b14d7b3702314fdf97dc737de1
Yunior <juniorliban@hotmail.com>
2018-12-16 10:24:19 -0500

    Seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,45 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/sockets';
 
 
 export default class Server {
+    public static _intance: Server;
+
     public app: express.Application;
     public port:Number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+   private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+        this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._intance || (this._intance = new this());
+    }
+
+    private escucharSockets() {
+    
+        console.log(`Escuchando conexiones - sockets`);
+        this.io.on('connection', cliente => {
+            //Mensajes
+            socket.mensaje(cliente, this.io);
+           //Desconectar
+           console.log('Cliente conectado'); 
+           socket.desconectar(cliente);
+        })
     }
 
     start(calllback: Function){
-        this.app.listen(this.port, calllback); 
+        this.httpServer.listen(this.port, calllback); 
     }
 }
